b2f0da1cb20e22d9e3734ac480a4804d9009b60c
tlfh04 <126224780+tlfh04@users.noreply.github.com>
2026-01-16 10:28:37 +0900

    file service 추가

---
+++ src/main/java/com/example/galleryback/service/FileStorageService.java
@@ -0,0 +1,8 @@
+package com.example.galleryback.service;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public interface FileStorageService {
+    String upload(MultipartFile file);
+    void delete(String imageUrl);
+}
---
+++ src/main/java/com/example/galleryback/service/LocalFileStorageService.java
@@ -0,0 +1,43 @@
+package com.example.galleryback.service;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.UUID;
+
+@Service
+public class LocalFileStorageService implements FileStorageService {
+
+    @Value("${upload.path}")
+    private String uploadPath;
+
+    @Override
+    public String upload(MultipartFile file) {
+        try{
+            String fileName = UUID.randomUUID() + "-" + file.getOriginalFilename();
+            Path filePath = Paths.get(uploadPath, fileName);
+            Files.createDirectories(filePath.getParent());
+            Files.write(filePath,file.getBytes());
+            return "/uploads/" + fileName;
+        }catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void delete(String imageUrl){
+
+        try{
+            Path filePath = Paths.get(uploadPath, imageUrl.replace("/uploads/",""));
+            Files.deleteIfExists(filePath);
+        }catch (IOException e){
+            throw new RuntimeException(e);
+        }
+    }
+}
