4ac1dc9f9dc0e46ed58536bd78567ab769affd1e
tlfh04 <126224780+tlfh04@users.noreply.github.com>
2026-01-16 09:35:56 +0900

    DB 설정

---
+++ src/test/java/com/example/galleryback/GalleryBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.galleryback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GalleryBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
