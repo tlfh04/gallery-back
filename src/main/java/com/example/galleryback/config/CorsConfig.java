6691e1fe9d2b5fdd83239360f66471a2b930d5f2
tlfh04 <126224780+tlfh04@users.noreply.github.com>
2026-01-16 11:19:21 +0900

    CorsConfig 추가

---
+++ src/main/java/com/example/galleryback/config/CorsConfig.java
@@ -0,0 +1,22 @@
+package com.example.galleryback.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/uploads/**")
+                .allowedOrigins("http://localhost:5173") // Replace with your frontend URL
+                .allowedMethods("*")
+                .allowedHeaders("*");
+
+        registry.addMapping("/api/**")
+                .allowedOrigins("http://localhost:5173")
+                .allowedMethods("*")
+                .allowedHeaders("*");
+    }
+
+}
